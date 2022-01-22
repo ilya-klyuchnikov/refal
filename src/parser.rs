e02fe0744b501cc8398bbd3ca12380789e5a180d
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2022-01-22 22:02:19 +0000

    initial usage of tree-sitter-refal

--- src/parser.rs
+++ src/parser.rs
@@ -1,158 +1,81 @@
-use crate::data::*;
-use crate::lexer;
-use crate::lexer::Token;
-use std::vec::IntoIter;
+use crate::data::RefalObject::*;
+use crate::data::{Error, Function, RefalModule, RefalObject, Result, Sentence};
+use tree_sitter::{Node, TreeCursor};
 
-pub fn parse_input(input: &str) -> Result<RefalModule> {
-    let tokens = lexer::tokens(input)?;
-    parse_module(tokens)
-}
-
-pub fn parse_module(tokens: Vec<Token>) -> Result<RefalModule> {
-    let PreModule(name, pre_funcs) = pre_parse_module(tokens)?;
-    let functions: Result<_> = pre_funcs.into_iter().map(function).collect();
-    Ok(RefalModule {
-        name,
-        functions: functions?,
-    })
-}
-
-fn pre_parse_module(tokens: Vec<Token>) -> Result<PreModule> {
-    let input: &mut IntoIter<Token> = &mut tokens.into_iter();
-
-    module(input)?;
-    let module_name = symbol(input)?;
-    semi(input)?;
-    let mut pre_funcs = Vec::<PreFunc>::new();
-
-    loop {
-        let pre_func_opt = pre_func(input)?;
-        match pre_func_opt {
-            None => break,
-            Some(pre_func) => pre_funcs.push(pre_func),
-        }
-    }
-    Ok(PreModule(module_name, pre_funcs))
-}
-
-struct PreModule(String, Vec<PreFunc>);
-struct PreFunc(String, Vec<Token>);
-
-fn function(p_func: PreFunc) -> Result<Function> {
-    let PreFunc(name, toks) = p_func;
-    let mut sentences = Vec::<Sentence>::new();
-    let tokens: &mut IntoIter<Token> = &mut toks.into_iter();
-    loop {
-        let sentence_opt = sentence(tokens)?;
-        match sentence_opt {
-            None => break,
-            Some(s) => {
-                sentences.push(s);
-            }
-        }
-    }
-    Ok(Function { name, sentences })
-}
-
-fn sentence(input: &mut IntoIter<Token>) -> Result<Option<Sentence>> {
-    let mut pattern = Vec::<RefalObject>::new();
-    let mut read = false;
-    loop {
-        match input.next() {
-            None if read => return Err(Error::Parsing),
-            None => return Ok(None),
-            Some(Token::Eq) => break,
-            Some(t) => {
-                read = true;
-                pattern.push(refal_object(t)?)
-            }
-        }
-    }
-
-    let mut expression: Vec<RefalObject> = Vec::<RefalObject>::new();
-    loop {
-        match input.next() {
-            None => return Err(Error::Parsing),
-            Some(Token::Semi) => break,
-            Some(t) => expression.push(refal_object(t)?),
-        }
-    }
-    Ok(Some(Sentence {
-        pattern,
-        expression,
-    }))
-}
-
-fn refal_object(token: Token) -> Result<RefalObject> {
-    match token {
-        Token::Semi | Token::Eq | Token::CurlyL | Token::CurlyR | Token::Module => {
-            Err(Error::Parsing)
-        }
-        Token::ParenL => Ok(RefalObject::StrBracketL),
-        Token::ParenR => Ok(RefalObject::StrBracketR),
-        Token::AngleL => Ok(RefalObject::FunBracketL),
-        Token::AngleR => Ok(RefalObject::FunBracketR),
-        Token::Symbol(s) => Ok(RefalObject::Symbol(s)),
-        Token::EVar(s) => Ok(RefalObject::EVar(s)),
-        Token::SVar(s) => Ok(RefalObject::SVar(s)),
-        Token::TVar(s) => Ok(RefalObject::TVar(s)),
+pub fn parse_input(text: &str) -> Result<RefalModule> {
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(tree_sitter_refal::language()).unwrap();
+    let tree = parser.parse(text, None).unwrap();
+    if tree.root_node().has_error() {
+        Err(Error::Parsing)
+    } else {
+        Ok(translate_module(&mut tree.walk(), text))
     }
 }
 
-fn pre_func(input: &mut IntoIter<Token>) -> Result<Option<PreFunc>> {
-    let token_opt = input.next();
-    match token_opt {
-        None => Ok(None),
-        Some(Token::Symbol(name)) => {
-            let body: Vec<Token> = pre_func_body(input)?;
-            Ok(Some(PreFunc(name, body)))
-        }
-        _ => Err(Error::Parsing),
+fn translate_module(cursor: &mut TreeCursor, text: &str) -> RefalModule {
+    let root_node = cursor.node();
+    let module_node = root_node.child_by_field_name("module").unwrap();
+    let name = get_name(&module_node, text);
+    let function_nodes: Vec<_> = root_node
+        .children_by_field_name("function", cursor)
+        .collect();
+    let functions: Vec<_> = function_nodes
+        .iter()
+        .map(|n| translate_function(n, cursor, text))
+        .collect();
+    RefalModule { name, functions }
+}
+
+fn translate_function<'a>(node: &Node<'a>, cursor: &mut TreeCursor<'a>, text: &str) -> Function {
+    let name = get_name(node, text);
+    let sentence_nodes: Vec<_> = node.children_by_field_name("sentence", cursor).collect();
+    let sentences: Vec<_> = sentence_nodes
+        .iter()
+        .map(|n| translate_sentence(n, cursor, text))
+        .collect();
+    Function { name, sentences }
+}
+
+fn translate_sentence<'a>(node: &Node<'a>, cursor: &mut TreeCursor<'a>, text: &str) -> Sentence {
+    let pattern: Vec<_> = node
+        .children_by_field_name("pattern", cursor)
+        .map(|n| translate_object(n, text))
+        .collect();
+    let rewrite: Vec<_> = node
+        .children_by_field_name("rewrite", cursor)
+        .map(|n| translate_object(n, text))
+        .collect();
+    Sentence { pattern, rewrite }
+}
+
+fn translate_object(node: tree_sitter::Node, text: &str) -> RefalObject {
+    match node.kind() {
+        "e_var" => EVar(get_string(&node, text)),
+        "s_var" => SVar(get_string(&node, text)),
+        "t_var" => TVar(get_string(&node, text)),
+        "id" => Symbol(get_string(&node, text)),
+        "q_symbol" => Symbol(get_string_stripped(&node, text)),
+        "str_br_l" => StrBracketL,
+        "str_br_r" => StrBracketR,
+        "fun_br_l" => FunBracketL,
+        "fun_br_r" => FunBracketR,
+        _ => panic!(),
     }
 }
 
-fn pre_func_body(input: &mut IntoIter<Token>) -> Result<Vec<Token>> {
-    curly_l(input)?;
-    let mut body = Vec::<Token>::new();
-    loop {
-        match input.next() {
-            None => return Err(Error::Parsing),
-            Some(token) => match token {
-                Token::CurlyR => return Ok(body),
-                t => body.push(t),
-            },
-        }
-    }
+fn get_name(node: &tree_sitter::Node, text: &str) -> String {
+    let name_node = node.child_by_field_name("name").unwrap();
+    text[name_node.byte_range()].to_string()
 }
 
-fn module(input: &mut IntoIter<Token>) -> Result<()> {
-    let token = input.next().ok_or(Error::Parsing)?;
-    match token {
-        Token::Module => Ok(()),
-        _ => Err(Error::Parsing),
-    }
+fn get_string(node: &tree_sitter::Node, text: &str) -> String {
+    text[node.byte_range()].to_string()
 }
 
-fn symbol(input: &mut IntoIter<Token>) -> Result<String> {
-    let token = input.next().ok_or(Error::Parsing)?;
-    match token {
-        Token::Symbol(s) => Ok(s),
-        _ => Err(Error::Parsing),
-    }
-}
-
-fn semi(input: &mut IntoIter<Token>) -> Result<()> {
-    let token = input.next().ok_or(Error::Parsing)?;
-    match token {
-        Token::Semi => Ok(()),
-        _ => Err(Error::Parsing),
-    }
-}
-
-fn curly_l(input: &mut IntoIter<Token>) -> Result<()> {
-    let token = input.next().ok_or(Error::Parsing)?;
-    match token {
-        Token::CurlyL => Ok(()),
-        _ => Err(Error::Parsing),
-    }
+fn get_string_stripped(node: &tree_sitter::Node, text: &str) -> String {
+    let mut range = node.byte_range();
+    range.start += 1;
+    range.end -= 1;
+    text[range].to_string()
 }
